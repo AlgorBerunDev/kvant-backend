5414b292c7a7785535b6456f32d89b8f8a13f871
Doston Xusanov <algorberun@gmail.com>
2023-03-10 19:24:32 +0500

    Added database cleaner example

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -8,11 +8,28 @@ describe('UserService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [PrismaModule],
+      imports: [PrismaModule.forRoot()],
       providers: [PrismaService, UserService],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    const prisma = module.get<PrismaService>(PrismaService);
+
+    const tableNames = await prisma.$queryRaw<
+      Array<{ tablename: string }>
+    >`SELECT tablename FROM pg_tables WHERE schemaname='public'`;
+
+    const tables = tableNames
+      .map(({ tablename }) => tablename)
+      .filter((name) => name !== '_prisma_migrations')
+      .map((name) => `"public"."${name}"`)
+      .join(', ');
+
+    try {
+      await prisma.$executeRawUnsafe(`TRUNCATE TABLE ${tables} CASCADE;`);
+    } catch (error) {
+      console.log({ error });
+    }
   });
 
   it('should be defined', async () => {
