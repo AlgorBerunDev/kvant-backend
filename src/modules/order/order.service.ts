337bfcf37eff9001b8ed0f79e42dcbfe4e523c35
Doston Xusanov <algorberun@gmail.com>
2023-06-26 11:58:13 +0500

    add sync remote payment service

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -9,15 +9,30 @@ import { UpdateOrderDto } from './dto/update-order.dto';
 import { OrderStatus } from './order.const';
 import { CreateOrderDetailDto } from './dto/create-order-detail.dto';
 import { UpdateOrderDetailDto } from './dto/update-order-detail.dto';
+import axios from 'axios';
 
 @Injectable()
 export class OrderService {
   constructor(private prisma: PrismaService) {}
 
-  create(createOrderDto: CreateOrderDto) {
-    return this.prisma.order.create({
+  async create(createOrderDto: CreateOrderDto) {
+    const created = await this.prisma.order.create({
       data: { ...createOrderDto, status: OrderStatus.InCart },
     });
+    await axios({
+      url: 'http://click.fibro.uz/api/order',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        original_id: created.id,
+        total_price: 0,
+        original_created_at: created.createdAt,
+        original_updated_at: created.updatedAt,
+      },
+    });
+    return created;
   }
 
   async addOrderDetail(orderDetailDto: CreateOrderDetailDto) {
@@ -25,11 +40,40 @@ export class OrderService {
       where: { id: orderDetailDto.productId },
     });
 
-    return this.prisma.orderDetail.create({
+    const addOrderDetail = await this.prisma.orderDetail.create({
       data: { ...orderDetailDto, price: product.price },
     });
+    await this.updateRemoteOrder(addOrderDetail.orderId);
+    return addOrderDetail;
   }
 
+  async updateRemoteOrder(orderId) {
+    const order = await this.prisma.order.findFirst({ where: { id: orderId } });
+    const allOrderDetails = await this.prisma.orderDetail.findMany({
+      where: { orderId: orderId },
+    });
+
+    let sum = 0;
+    allOrderDetails.forEach((orderDetail) => {
+      sum = sum + orderDetail.price * orderDetail.quantity;
+    });
+
+    await axios({
+      url: 'http://click.fibro.uz/api/order/' + orderId,
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        original_id: orderId,
+        total_price: sum,
+        original_created_at: order.createdAt,
+        original_updated_at: order.updatedAt,
+      },
+    });
+
+    return sum;
+  }
   async updateOrderDetail(
     orderDetailId: number,
     orderDetailDto: UpdateOrderDetailDto,
@@ -42,20 +86,36 @@ export class OrderService {
       throw new NotFoundException();
     }
 
-    return await this.prisma.orderDetail.update({
+    const updatedOrderDetail = await this.prisma.orderDetail.update({
       where: { id: orderDetailId },
       data: orderDetailDto,
     });
+
+    await this.updateRemoteOrder(updatedOrderDetail.orderId);
+    return updatedOrderDetail;
   }
 
-  removeOrderDetail(orderDetailId: number) {
-    return this.prisma.orderDetail.deleteMany({ where: { id: orderDetailId } });
+  async removeOrderDetail(orderDetailId: number) {
+    const orderDetail = await this.prisma.orderDetail.findFirst({
+      where: { id: orderDetailId },
+    });
+
+    const removedOrderDetail = await this.prisma.orderDetail.deleteMany({
+      where: { id: orderDetailId },
+    });
+
+    await this.updateRemoteOrder(orderDetail.orderId);
+
+    return removedOrderDetail;
   }
 
   orderDetails(id: number) {
     return this.prisma.orderDetail.findMany({
       where: { orderId: id },
-      include: { product: { include: { images: true } } },
+      include: {
+        product: { include: { images: true } },
+        order: { include: { user: true } },
+      },
     });
   }
 
